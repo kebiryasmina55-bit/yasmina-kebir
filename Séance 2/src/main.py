34d3423a087fe10da7ddf026bbb7cc3a2b5df384
kebiryasmina55-bit <kebiryasmina55@gmail.com>
2025-12-19 15:51:43 +0100

    Add files via upload

--- Séance 2/src/main.py
+++ Séance 2/src/main.py
@@ -10,65 +10,23 @@ with open("./data/resultats-elections-presidentielles-2022-1er-tour.csv","r") as
 # Mettre dans un commentaire le numéro de la question
 # Question 1
 
-# Affichage du tableau
-print(contenu)
-pd.dataFrame(contenu)
+df = pd.DataFrame(contenu)
+print (df)
+nb_lignes = len(contenu)
+nb_colonnes = len(contenu.columns)
 
-# Nombre de lignes et colonnes
-print("Nombre de lignes :", len(contenu))
-print("Nombre de colonnes :", len(contenu.columns))
+print (nb_lignes)
+print (nb_colonnes)
 
-# Types de colonnes
-types_colonnes = {}
-for col in contenu.columns:
-    types_colonnes[col] = contenu[col].dtype
-print("Types de colonnes :", types_colonnes)
+print (df.dtypes)
 
-# Affichage des noms de colonnes
-print("Noms des colonnes :", contenu.columns.tolist())
+en_tete_colonne = df.head(0)
+print (en_tete_colonne)
 
-# Sélection du nombre d'inscrits
-if 'Inscrits' in contenu.columns:
-    inscrits = contenu['Inscrits']
-    print("Inscrits :", inscrits)
+inscrits = df ["Inscrits"]
+print(inscrits)
 
-# Calcul des effectifs pour les colonnes quantitatives
-effectifs = []
-for col in contenu.columns:
-    if contenu[col].dtype in ['int64', 'float64']:
-        effectifs.append((col, contenu[col].sum()))
-print("Effectifs :", effectifs)
-
-# Création du dossier images
-os.makedirs('images', exist_ok=True)
-
-# Diagrammes en barres pour inscrits et votants
-for i, row in contenu.iterrows():
-    departement = row['Code département']
-    valeurs = [row['Inscrits'], row['Votants']]
-    noms = ['Inscrits', 'Votants']
-    plt.bar(noms, valeurs)
-    plt.title(f'Département {departement}')
-    plt.savefig(f'images/bar_{departement}.png')
-    plt.clf()
-
-# Diagrammes circulaires pour blancs, nuls, exprimés, abstention
-for i, row in contenu.iterrows():
-    departement = row['Code département']
-    valeurs = [row['Blancs'], row['Nuls'], row['Exprimés'], row['Abstentions']]
-    noms = ['Blancs', 'Nuls', 'Exprimés', 'Abstentions']
-    plt.pie(valeurs, labels=noms, autopct='%1.1f%%')
-    plt.title(f'Département {departement}')
-    plt.savefig(f'images/pie_{departement}.png')
-    plt.clf()
-
-# Histogramme de la distribution des inscrits
-plt.hist(contenu['Inscrits'], bins='sturges', density=True)
-plt.title('Distribution des inscrits')
-plt.xlabel('Nombre d’inscrits')
-plt.ylabel('Densité')
-plt.savefig('images/histogramme_inscrits.png')
-plt.clf()
-
+typedescolonnes = ["str", "str", "int", "int", "int", "int", "int", "int", "str", "str", "str", "int", "str", "str", "str", "int", "str", "str", "str", "int", "str", "str", "str", "int", "str", "str", "str", "int", "str", "str", "str", "int", "str", "str", "str", "int", "str", "str", "str", "int", "str", "str", "str", "int", "str", "str", "str", "int", "str", "str", "str", "int", "str", "str", "str", "int" ]
+for 
 
 
